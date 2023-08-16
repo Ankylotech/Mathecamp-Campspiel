e0d796e6191002eeeacb70f7d5139286a507dc5a
Jonas <jonas.rubeck@gmail.com>
2023-08-16 15:29:24 +0200

    Nullstellen operation

--- aufgaben_campspiel.py
+++ aufgaben_campspiel.py
@@ -1,23 +1,32 @@
 # Importiere Funktionen, die eine Aufgabe zu einer bestimmten Lösung generieren
+import random
+
 from operations import rand_ops
 from fruit_math import fruit_math
+from nullstellen import nst
 # Zufällige Auswahl von Lösungen und Generatoren
 from random import choice
 from random import sample
 
 # Anzahl Probleme und Lösungen
-problems = 16
+problems = 24
 solution_groups = 8
 
 # Welche Funktionen verwendet werden um Aufgaben zu generieren
-generators = [rand_ops, fruit_math]
+generators = [rand_ops, fruit_math, nst]
 # Verschiedenen Lösungen
 # solutions = [1,2,3,4,5,6,7,8]
-solutions = sample(range(30, 250), solution_groups)
+solutions = [0, 1, 314, 2718, 6283, 10, 2, 299792458]
 
 # Generiere für jede Lösung gleich viele Probleme zufällig aus den Generator-Funktionen
 aufgaben = [[(choice(generators)(x), x) for x in solutions] for _ in range(int(problems / len(solutions)))]
 aufgaben = [x for sublist in aufgaben for x in sublist]
+
+nums = [i for i in range(1, problems+1)]
+random.shuffle(nums)
+s = {}
+for n in solutions:
+    s[str(n)] = []
 
 # Speichere die Aufgaben un einer Datei
 with open('output.txt', 'w') as f:
@@ -26,11 +35,17 @@ with open('output.txt', 'w') as f:
     f.write('\n')
     f.write('\n')
     for aufgabe in aufgaben:
+        num = nums.pop()
+        s[str(aufgabe[1])].append(num)
         f.write("problem: \n")
         f.write(aufgabe[0])
+        f.write('\n')
+        f.write("number: ")
+        f.write(str(num))
         f.write('\n')
         f.write("solution: ")
         f.write(str(aufgabe[1]))
         f.write('\n')
         f.write('\n')
         f.write('\n')
+    f.write(str(s))
---
+++ nullstellen.py
@@ -0,0 +1,39 @@
+from random import randrange
+from random import uniform
+import numpy
+
+# Spalte n in k Zufallszahlen, die auf n Summieren
+def split(n,k):
+    result = []
+    left = k
+    while left > 1:
+        num = randrange(5,100)
+        if uniform(0, 1) < 0.5:
+            num *= -1
+
+        n -= num
+        left -= 1
+        result.append(num)
+    result.append(n)
+    return result
+
+
+def coef_test(polynomial):
+    for c in polynomial.coef:
+        if str(c)[:-2] != str(int(c)):
+            return False
+    return True
+
+
+def nst(solution):
+    degree = randrange(5, 7)
+    nsts = split(solution, degree)
+    polynomial = numpy.polynomial.Polynomial.fromroots(nsts)
+    while not coef_test(polynomial):
+        nsts = split(solution, degree)
+        polynomial = numpy.polynomial.Polynomial.fromroots(nsts)
+    result = "Summiere die Ganzzahligen Nullstellen des folgenden Polynoms: \n"
+    result += str(polynomial)
+    result += "\n"
+    return result
+
